f7d35c65e6749539ad0def8ac4b41d28ab04fdb4
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-03-10 23:55:44 +0530

    2nd file adding

---
+++ src/ex_01_Java_Basic/Lab02_CommentHandson.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab02_CommentHandson {
+    public static void main(String[] args) {
+        //System.out.println("Single Line comment");
+        /*System.out.println("Multi Line comment1");
+        System.out.println("Multi Line comment2");
+        System.out.println("Multi Line comment3")*/
+        System.out.println("No Line comment");
+    }
+
+
+}
