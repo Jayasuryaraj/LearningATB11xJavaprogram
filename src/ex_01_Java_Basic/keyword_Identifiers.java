e6c1808c2e2383702e8593da841daa37a9311377
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-03-10 23:52:54 +0530

    2nd file adding

---
+++ src/ex_01_Java_Basic/keyword_Identifiers.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class keyword_Identifiers {
+    public static void main(String[] args) {
+        //package,public class, public static void - Keywords
+        //ex_01_Java_Basic, keyword_Identifiers- Identifiers
+    }
+}
