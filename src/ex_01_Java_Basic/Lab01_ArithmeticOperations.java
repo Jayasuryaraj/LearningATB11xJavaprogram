888a4cef8fdb9d570e894e3785d910f15eb3d970
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-03-10 23:55:44 +0530

    10th March Task

---
+++ src/ex_01_Java_Basic/Lab01_ArithmeticOperations.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab01_ArithmeticOperations {
+    public static void main(String[] args) {
+        System.out.println(3+2);
+        System.out.println(3-12);
+        System.out.println(10/2);
+        System.out.println(5*2);
+    }
+}
