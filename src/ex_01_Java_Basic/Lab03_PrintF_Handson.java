2478c26caf722c8d21a7820ab652742d5c9921a8
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-03-11 23:57:01 +0530

    11 march PrintF Handson

---
+++ src/ex_01_Java_Basic/Lab03_PrintF_Handson.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basic;
+
+public class Lab03_PrintF_Handson {
+    public static void main(String[] args) {
+        int a= 9;
+        System.out.printf("Value of 9*1=%d\n",a);
+        System.out.printf("Value of 9*2=%d\n",a*2);
+        System.out.printf("Values is 9*3=%d\n",a*3);
+        System.out.printf("Values is 9*4=%d\n",a*4);
+        System.out.printf("Values is 9*5=%d\n",a*5);
+        System.out.printf("Values is 9*6=%d\n",a*6);
+        System.out.printf("Values is 9*7=%d\n",a*7);
+        System.out.printf("Values is 9*8=%d\n",a*8);
+        System.out.printf("Values is 9*9=%d\n",a*9);
+        System.out.printf("Values is 9*10=%d\n",a*10);
+
+
+    }
+}
