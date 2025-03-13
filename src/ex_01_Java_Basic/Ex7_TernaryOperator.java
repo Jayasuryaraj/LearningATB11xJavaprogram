fbc7aae159fa56060af8da5c364a4dcdc7575c67
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-03-14 00:15:37 +0530

    13 March Ternary Operator

---
+++ src/ex_01_Java_Basic/Ex7_TernaryOperator.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Ex7_TernaryOperator {
+    public static void main(String[] args) {
+        int a=60;
+        String Category= a>=60 ? "Yes, Senior Citizen" : "Not a Senior citizen";
+        System.out.println(Category);
+    }
+}
