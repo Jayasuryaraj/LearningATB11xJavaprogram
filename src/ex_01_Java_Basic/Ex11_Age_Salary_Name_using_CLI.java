cb54a13bbf73e8d60cd785086223679f555ac7e3
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-03-18 13:01:36 +0530

    14 Mar Age Name Salary CLI command

--- src/ex_01_Java_Basic/Ex11_Age_Salary_Name_using_CLI.java
+++ src/ex_01_Java_Basic/Ex11_Age_Salary_Name_using_CLI.java
@@ -6,11 +6,21 @@ public class Ex11_Age_Salary_Name_using_CLI {
         String Salary = args[1];
         String Name = args[2];
 
-
         System.out.println("Your Name is : "+ Name);
         System.out.println("Your Age is : "+ Age);
         System.out.println("Your Salary is "+ Salary);
+    /*
+    Or
 
+    public static void main(String[] args) {
+        String Age = args[0];
+        String Salary = args[1];
+        String Name = args[2];
+
+        System.out.println("Your Name is : "+ Name+"\n" + "Age is : " +Age+"\n" + "Salary is : " +Salary);
+
+
+     */
 
     }
 }
