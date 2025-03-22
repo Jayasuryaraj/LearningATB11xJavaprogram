1e86f66ad4c5c715c0883dd8f83dc35435ea0923
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-03-22 10:30:45 +0530

    14 Mar Age Salary Name using CLI

--- src/Task/Ex11_Age_Salary_Name_using_CLI.java
+++ src/Task/Ex11_Age_Salary_Name_using_CLI.java
@@ -2,13 +2,14 @@ package Task;
 
 public class Ex11_Age_Salary_Name_using_CLI {
     public static void main(String[] args) {
-        String Age = args[0];
-        String Salary = args[1];
+
+        int Age1 = Integer.parseInt(args[0]);
+        double Salary1 = Double.parseDouble(args[1]);
         String Name = args[2];
 
         System.out.println("Your Name is : "+ Name);
-        System.out.println("Your Age is : "+ Age);
-        System.out.println("Your Salary is "+ Salary);
+        System.out.println("Your Age is : "+ Age1);
+        System.out.println("Your Salary is "+ Salary1);
     /*
     Or
 
