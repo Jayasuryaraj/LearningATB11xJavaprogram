d28252b34fd8920ded7e9cbedee25307753603b5
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-03-22 13:51:16 +0530

    14 Mar For loop_without condition

---
+++ src/Forloop/Ex_13_For_loop_without_Condition.java
@@ -0,0 +1,10 @@
+package Forloop;
+
+public class Ex_13_For_loop_without_Condition {
+    public static void main(String[] args) {
+        //without Condition >> It will run infinitely
+        for(int i=0; /*No condition*/ ; i++ )
+        {
+            System.out.println("Hello");
+        }
+    }}
