f49b04bb1cadb3f57a36c0cb0a467478d71fd9c8
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-03-22 13:50:54 +0530

    14 Mar For loop_without condition

--- src/Forloop/Ex_for_loop.java
+++ src/Forloop/Ex_for_loop.java
@@ -2,7 +2,7 @@ package Forloop;
 
 public class Ex_for_loop {
     public static void main(String[] args) {
-        for(int _1=0; _1<10; _1++){
-        System.out.println(_1);}
+        for(int i=0; i<10; i++){
+        System.out.println(i);}
     }
 }
