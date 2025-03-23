25c3986ca10732fe4c8603d6bf4d347f3f74c935
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-03-23 21:53:37 +0530

    20 Mar-For_loop with Even & Odd number

--- src/Forloop/Ex_for_loop.java
+++ src/Forloop/Ex_for_loop.java
@@ -3,6 +3,7 @@ package Forloop;
 public class Ex_for_loop {
     public static void main(String[] args) {
         for(int i=0; i<10; i++){
-        System.out.println(i);}
+           System.out.println(i);
+        }
     }
 }
