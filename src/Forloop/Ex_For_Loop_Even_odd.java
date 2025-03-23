71af83e1e2ef8b1d0641c3e4162a695d458e9f91
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-03-23 21:53:53 +0530

    20 Mar-For_loop with Even & Odd number

---
+++ src/Forloop/Ex_For_Loop_Even_odd.java
@@ -0,0 +1,13 @@
+package Forloop;
+
+public class Ex_For_Loop_Even_odd {
+    public static void main(String[] args) {
+        for(int i=0; i<=100; i++)
+        {
+             /*To find odd number use if (i%2==1)
+             to find Even number use if(i%2==0)*/
+            if(i%2==1)
+            System.out.println(i);
+        }
+    }
+}
