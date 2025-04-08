f49869b14bd79a95681802045de770acc4235e57
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-04-08 19:51:15 +0530

    27 Mar_Arrays

---
+++ src/Arrays/Arrays_Print.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Arrays_Print {
+    public static void main(String[] args) {
+        int[] marks={90,70,76,80,93,83, 78};
+            for (int i = 0; i <marks.length ; i++) {
+            System.out.println(marks[i]); // [] is used to find the index of values
+
+//            String[] Students= {"Raja","Jaysu","Surya", "Mani"};
+//            for (int j = 0; j <Students.length ; j++) {
+//                System.out.println(Students[j]);
+//
+//            }
+        }
+    }
+}
