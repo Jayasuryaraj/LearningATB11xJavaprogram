8ff7c0c94c1fb32bd0429b13e8b425c9720071a9
Jayasuryaraj <jayasuryaraj98k@gmail.com>
2025-04-19 23:16:00 +0530

    1 April Default Constructor

---
+++ src/OOPS_Example/OOPs_constructor.java
@@ -0,0 +1,24 @@
+package OOPS_Example;
+
+public class OOPs_constructor {
+    public static void main(String[] args) {
+
+       Student S1 = new Student(); //Object1
+    //new Student(); //Object2
+
+    }
+}
+//Attribute
+    class Student {
+    String Name;
+
+    //Behaviour
+    void learning(){
+
+    }
+    //Default Constructor
+    Student (){
+    System.out.println("Hello Ji");
+}
+}
+
